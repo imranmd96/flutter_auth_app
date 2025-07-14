87084691e15f3451d592aea284aed81520ce5d62
imran ahmed <imranmd96@gamil.com>
2025-07-14 02:21:21 +0200

    Aggressive repository cleanup for Railway deployment
    - Remove all node_modules directories
    - Remove all log files and .DS_Store files
    - Remove Flutter build artifacts and platform files
    - Remove large JAR files and build outputs
    - Update .gitignore with comprehensive exclusions
    - Focus repository on backend services only

--- devops/ci-cd/lib copy/bottom-navbar/controllers/bottom_nav_controller.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-
-class BottomNavController extends GetxController {
-  final RxInt selectedIndex = 0.obs;
-
-  void changeIndex(int index) {
-    selectedIndex.value = index;
-  }
-} 
--- web/main.dart
+++
@@ -1,5 +0,0 @@
-import 'package:my_flutter_app/main.dart' as app;
-
-void main() {
-  app.main();
-} 
